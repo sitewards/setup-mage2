b6990b0de225c9b6f52d5c01e55f9bf6174ffd4b
ironm <poposki.darko@gmail.com>
2017-01-04 16:46:08 +0000

    Extract the Mage2 specific part to its own module

---
+++ src/Sitewards/Setup/Persistence/PageRepository.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * This file is part of the Setup package.
+ *
+ * (c) Sitewards GmbH
+ */
+
+namespace Sitewards\Setup\Persistence;
+
+use Sitewards\Setup\Domain\Page;
+
+class PageRepository implements PageRepositoryInterface
+{
+    /**
+     * @var \Magento\Cms\Api\PageRepositoryInterface
+     */
+    private $pageRepository;
+
+    /**
+     * @var \Magento\Framework\Api\SearchCriteriaBuilder
+     */
+    private $searchCriteria;
+
+    /**
+     * @param \Magento\Cms\Api\PageRepositoryInterface $pageRepository
+     * @param \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteria
+     */
+    public function __construct(
+        \Magento\Cms\Api\PageRepositoryInterface $pageRepository,
+        \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteria
+    ) {
+        $this->pageRepository = $pageRepository;
+        $this->searchCriteria = $searchCriteria;
+    }
+
+    /**
+     * @param array $ids
+     *
+     * @return Page[]
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function findByIds(array $ids)
+    {
+        $this->setIdFilter($ids);
+
+        return $this->findItems();
+    }
+
+    /**
+     * @return Page[]
+     */
+    public function findAll()
+    {
+        return $this->findItems();
+    }
+
+    /**
+     * @return Page[]
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    private function findItems()
+    {
+        /** @var \Magento\Framework\Api\SearchResults $results */
+        $results = $this->pageRepository->getList($this->searchCriteria->create());
+
+        $pages = [];
+
+        foreach ($results->getItems() as $page) {
+            $pages[] = new Page(
+                $page['identifier'],
+                $page['content'],
+                $page['active']
+            );
+        }
+
+        return $pages;
+    }
+
+    /**
+     * Set the filter ids
+     *
+     * @param array $ids
+     */
+    private function setIdFilter(array $ids)
+    {
+        if (!empty($ids)) {
+            $this->searchCriteria->addFilter(
+                'identifier',
+                implode(',', $ids),
+                'in'
+            );
+        }
+    }
+}
